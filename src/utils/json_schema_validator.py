1282279f462779866b97567c327fa5ac15b7c63f
Rossby <verofesa@gmail.com>
2018-12-21 08:57:09 -0400

    start

---
+++ src/utils/json_schema_validator.py
@@ -0,0 +1,14 @@
+from jsonschema import validate
+
+from src.utils.LoggerHandler import LoggerHandler
+
+logger = LoggerHandler.get_instance()
+
+def validate_json_schema(schema, json):
+    try:
+        validate(json, schema)
+        logger.info("Validate Schema")
+        return None, True
+    except Exception as e:
+        logger.info("FAILED JSON SCHEMA validation: {}".format(e.message))
+        return False, e.message
