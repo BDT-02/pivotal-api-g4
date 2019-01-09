66d01e96b75b81fd68578a8ea6326296f717e5f1
Gabriel Duran <gabodc20@gmail.com>
2019-01-09 09:30:12 -0400

    Updates

--- features/steps/workspaces_steps.py
+++ features/steps/workspaces_steps.py
@@ -14,13 +14,13 @@ def create_project_step(context):
     context.workspaces_status, context.workspaces_response = workspaces_services.create_workspace(data)
 
 
-@then(u'I verify project creation status is {status_code}')
+@then(u'I verify workspace creation status is {status_code}')
 def step_impl(context, status_code):
     print(context.project_status)
     assert context.workspaces_status == status_code, "Workspace creation status is %s" % status_code
 
 
-@step(u'I verify project schema')
+@step(u'I verify workspace schema')
 def step_impl(context):
     actual_response = workspaces_services.get_workspace(id=str(context.project_response["id"]))
     schema = workspaces_services.get_workspace_schema()
