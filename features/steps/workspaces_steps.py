15d581ac4458df63f47aa40df0bd427a5db01f0f
Gabriel Duran <gabodc20@gmail.com>
2019-01-11 08:00:01 -0400

    Update

--- features/steps/workspaces_steps.py
+++ features/steps/workspaces_steps.py
@@ -1,13 +1,77 @@
-import time
-
-from behave import Given, then, step
+from behave import given, when, then
 
 from src.pivotal_api_services.workspaces import WorkspaceServices
 from src.utils.json_schema_validator import validate_json_schema
 
-workspaces_services = WorkspaceServices()
+workspace_services = WorkspaceServices()
+other_workspaces = {"Non-Existent": "1", "Non-Authorized": "12E45"}
 
 
+@given("I have the workspaces list")
+def step_impl(context):
+    context.workspace_list = workspace_services.get_workspaces()
+
+
+@given("I created the workspace {name}")
+def create_workspace_step(context, name):
+    data = {"name": name}
+    context.response_status, context.workspace_response = workspace_services.create_workspace(data)
+
+
+@then("verify the {action_type} status is {status_code}")
+def verify_action_status(context, action_type, status_code):
+    assert context.response_status == int(status_code), \
+        "Workspace %s status is %s" % (action_type, status_code)
+
+
+@then("verify the workspace schema")
+def verify_workspace_schema(context):
+    workspace = workspace_services.get_workspace(id=str(context.workspace_response["id"])).json()
+    schema = workspace_services.get_workspace_schema()
+    is_schema_valid, schema_failure_reason = validate_json_schema(schema, workspace)
+    assert is_schema_valid, "Workspace Schema failed due to: {}".format(schema_failure_reason)
+
+
+@then("verify the workspace {name} exists")
+def verify_workspace_exists(context, name):
+    assert name in context.workspace_list, "Workspace {} is not in the list".format(name)
+
+
+@given("I selected {name} from the list")
+def step_impl(context, name):
+    context.workspace_id = other_workspaces[name] if name in other_workspaces \
+        else context.workspace_list[name]
+
+
+@when("I requested its details")
+def step_impl(context):
+    response = workspace_services.get_workspace(context.workspace_id)
+    context.workspace_response = response.json()
+    context.response_status = response.status_code
+    context.response_reason = response.reason
+
+
+@when("I requested delete it")
+def step_impl(context):
+    context.response_status, context.response_reason = \
+        workspace_services.delete_workspace(context.workspace_id)
+
+
+<<<<<<< HEAD
+@then('the response reason is "{reason}"')
+def step_impl(context, reason):
+    assert context.response_reason == reason, "Workspace response reason is '%s'" % reason
+
+
+@given("I delete all the workspaces")
+def delete_workspaces(context):
+    workspace_services.delete_all_workspaces()
+
+
+@then("verify the list is empty")
+def step_impl(context):
+assert not workspace_services.get_workspaces(), "There is still workspaces"
+=======
 @Given(u"I create a Workspace")
 def create_workspace_step(context):
     data = {"name": "New Workspace"}
@@ -26,3 +90,4 @@ def step_impl(context):
     schema = workspaces_services.get_workspace_schema()
     schema_failure_reason, is_schema_valid = validate_json_schema(schema, actual_response)
     assert is_schema_valid, "Workspace Schema failed due to: {}".format(schema_failure_reason)
+>>>>>>> 209d0053ba658a9d270aecf797a8041b45344928
--- features/workspaces.feature
+++ features/workspaces.feature
@@ -1,7 +1,49 @@
-Feature: showing off behave
+Feature: Work with workspaces
 
-  Scenario: Create new workspace
-    Given I create a workspace
-        | name | Workspace|
-    Then I verify workspace creation status is 200
-    And I verify workspace schema
+  Background:
+    Given I have the workspaces list
+
+  Scenario Outline: Create new workspace <name>
+    Given I created the workspace <name>
+    Then verify the creation status is 200
+    And verify the workspace schema
+    And verify the workspace <name> exists
+    Examples: Workspaces
+      | name         |
+      | Workspace_01 |
+      | Workspace_02 |
+      | Workspace_03 |
+
+  Scenario: Details of existent workspace
+    Given I selected Workspace_01 from the list
+    When I requested its details
+    Then verify the get status is 200
+    And verify the workspace schema
+
+  Scenario: Details of non-existent workspace
+    Given I selected Non-Existent from the list
+    When I requested its details
+    Then verify the get status is 404
+    And the response reason is "Not Found"
+
+  Scenario: Details of non-authorized workspace
+    Given I selected Non-Authorized from the list
+    When I requested its details
+    Then verify the get status is 403
+    And the response reason is "Forbidden"
+
+  Scenario: Delete a existent workspace
+    Given I selected Workspace_03 from the list
+    When I requested delete it
+    Then verify the deletion status is 204
+    And the response reason is "No Content"
+
+  Scenario: Delete a non-existent workspace
+    Given I selected Non-Existent from the list
+    When I requested delete it
+    Then verify the deletion status is 404
+    And the response reason is "Not Found"
+
+  Scenario: Delete all workspaces
+    Given I delete all the workspaces
+Then verify the list is empty
